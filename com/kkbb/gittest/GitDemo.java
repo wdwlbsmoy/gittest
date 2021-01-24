8f450382b5f8ef27e751a27f120082895a6b4c53
john <542530488@qq.com>
2021-01-24 20:19:01 +0800

    git test init

---
+++ com/kkbb/gittest/GitDemo.java
@@ -0,0 +1,13 @@
+package com.kkbb.gittest;
+
+/**
+ * @Auther:xueruiheng
+ * @Date:2021/1/24
+ * @Description:com.kkbb.gittest
+ * @version:1.0
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
