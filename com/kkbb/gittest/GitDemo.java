9c9a5497903a9a1971f61127e2ab8356cec97b6f
john <542530488@qq.com>
2021-01-24 20:43:52 +0800

    updata gittest

--- com/kkbb/gittest/GitDemo.java
+++ com/kkbb/gittest/GitDemo.java
@@ -9,5 +9,6 @@ package com.kkbb.gittest;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello git2!");
     }
 }
